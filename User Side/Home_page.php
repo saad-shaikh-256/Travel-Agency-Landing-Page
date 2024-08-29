6475b757ad3a84af93b75f09e535f143eddfaf43
Saad Shaikh <shaikhsaad256@gmail.com>
2024-08-29 22:51:26 +0530

    Hero Section Compelete but only web

---
+++ User Side/Home_page.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <!-- Meta Tags  -->
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- File Links -->
+   <link rel="stylesheet" href="Home_page.css">
+   <link rel="shortcut icon" href="../Materials/Logo/Favicon.png" type="image/x-icon">
+
+   <!-- External Links -->
+
+
+   <!-- Title Links -->
+   <title>Luzio Hotel - PHP Project</title>
+</head>
+
+<body>
+
+   <!-- Header Section -->
+   <header>
+      <nav>
+         <ul class="Logo">
+            <img src="../Materials/Logo/Logo.svg" alt="Luzio Logo">
+         </ul>
+         <ul>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="http://">Rooms</a></li>
+            <li><a href="http://">About</a></li>
+            <li><a href="http://">Services</a></li>
+            <li><a href="http://">Contact</a></li>
+         </ul>
+         <ul class="Profile">
+            <img src="../Materials/Icons/Profile Icon.svg" alt="Profile Icon">
+         </ul>
+      </nav>
+   </header>
+   <section id="Home"></section>
+   <div class="HeroSection">
+      <div class="HeroText">
+         <div class="HeroText1">
+            <h1>Unforgettable experiences start with
+               the perfect room. Browse our
+               collection and book now.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur. Facilisis sed ullamcorper in aliquam donec vel neque
+               aliquet. Penatibus eget.</p>
+         </div>
+         <a>Explore More</a>
+      </div>
+      <div class="HeroImage">
+         <img src="../Materials/Images/HeroImage.svg" alt="Hero Image">
+      </div>
+   </div>
+
+</body>
+
+</html>
