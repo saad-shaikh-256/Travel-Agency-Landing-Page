d2f998dd9321193e9ebc090de8f7b5536a871fe8
Saad Shaikh <shaikhsaad256@gmail.com>
2024-12-18 15:49:05 +0530

    Completed with GSAP

---
+++ script.js
@@ -0,0 +1,71 @@
+gsap.to("#logo",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
+
+gsap.to("#links",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
+gsap.to("#menu",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);gsap.to("#contact",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
+gsap.to("#upperText",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
+gsap.to("#heading",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
+gsap.to("#subheading",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
+gsap.to("#button",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);gsap.to("#rightContainer img",
+   {
+      y: "0%",
+      opacity: 1,
+      duration: 0.75,
+      ease: "power1.inOut",
+   }
+);
